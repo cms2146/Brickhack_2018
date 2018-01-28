3d0683c489cd58ebd32effc76e60fe8ce7ece730
chris <cms2146@rit.edu>
2018-01-27 19:23:55 -0500

    Deserializaer working for real now

--- TripMeBaby/rest/urls.py
+++ TripMeBaby/rest/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('deserializer/', views.deserializer),
+    path('deserializer', views.deserializer),
 ]
--- TripMeBaby/rest/views.py
+++ TripMeBaby/rest/views.py
@@ -1,9 +1,11 @@
 from django.utils.six import BytesIO
 from rest_framework.parsers import JSONParser
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework import serializers
+from django.http import HttpResponse
+@csrf_exempt
 def deserializer(json):
-	stream = BytesIO(json)
+	stream = BytesIO(json.body)
 	data = JSONParser().parse(stream)
-	serializer = CommentSerializer(data=data)
-	serializer.is_valid()
-	serializer.validated_data
-	print(data)
+	var = data["key"]
+	return HttpResponse(var)

f701bebd69941c5e9ba1ef3b7a5e741bdae34388
chris <cms2146@rit.edu>
2018-01-27 20:36:31 -0500

    first API call is almost made

--- TripMeBaby/api_calls/views.py
+++ TripMeBaby/api_calls/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from urllib.request import urlopen
+places_api_key = 'AIzaSyBMkM6kLxccjjZhtwVuGiA6roJTZz9AmoU '
+def get_places(location):
+	url = "https://maps.googleapis.com/maps/api/place/textsearch/json?query="
+	for i in location:
+		if i ==" ":
+			i = "+"
+	url +=location
+	url+= "&key="
+	url+= places_api_key
+	html = urlopen(url)
+	
 
-# Create your views here.
+

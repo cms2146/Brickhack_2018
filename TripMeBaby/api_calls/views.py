c037f0630652e78621701d4442bf5a723972d767
chris <cms2146@rit.edu>
2018-01-27 21:38:36 -0500

    Places api call finished

--- TripMeBaby/api_calls/views.py
+++ TripMeBaby/api_calls/views.py
@@ -3,11 +3,14 @@ from urllib.request import urlopen
 import rest.models
 places_api_key = 'AIzaSyBMkM6kLxccjjZhtwVuGiA6roJTZz9AmoU'
 def get_places(location):
-	url = "https://maps.googleapis.com/maps/api/place/textsearch/json?query=restaurants+in+Sydney&key=AIzaSyBMkM6kLxccjjZhtwVuGiA6roJTZz9AmoU"
-	location.replace(" ", "+")
+	url = 'https://maps.googleapis.com/maps/api/place/textsearch/json?query='
+	location =location.replace(" ", "+")
+	url +=location
+	url+='&key='
+	url+= places_api_key
+	print(url)
 	json = urlopen(url)
 	data = rest.models.basic_deserializer(json)
-	print(data['results'][1]['formatted_address'])
 	return data
 
 
